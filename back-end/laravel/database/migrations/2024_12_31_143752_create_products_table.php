55e294dbc9873c1b1b7a8012cc3b8880c2803b8f
mhainid <ayoubmhainid01@gmail.com>
2024-12-31 16:37:29 +0100

    migrations completed

---
+++ back-end/laravel/database/migrations/2024_12_31_143752_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('productName');
+            $table->text('description');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->unsignedBigInteger('store_id');
+            $table->foreign('store_id')->references('id')->on('stores')->onDelete('cascade');
+            $table->string('productImage');
+            $table->float('price');
+            $table->integer('likes');
+            $table->integer('stock');
+            $table->string('location');
+            $table->boolean('delivry');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
