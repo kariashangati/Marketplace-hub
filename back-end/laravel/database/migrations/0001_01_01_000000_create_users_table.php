3cfacd6785b51fc97ee6c8c3e102f03d13753915
said <saiid.kachoud@gmail.com>
2024-12-31 15:29:31 +0100

    migrations

--- back-end/laravel/database/migrations/0001_01_01_000000_create_users_table.php
+++ back-end/laravel/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('fullName');
+            $table->string('username')->unique();
+            $table->date('birthday')->nullable();
+            $table->enum('role',['admin','user']);
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->text('bio')->nullable();
+            $table->string('profile_picture')->nullable();
             $table->timestamps();
         });
 
